f5646e071703565876093c05c70ce33121dc6736
tunmight <tunmight111@gmail.com>
2023-05-23 21:57:27 +0100

    main

--- main.c
+++ main.c
@@ -1,13 +1,44 @@
-#include <stdio.h>
-#include "add.h"
-int add(int num1, int num2);
+#include "file.h"
 
-int main(void)
+/**
+ * main - entry point
+ * @ac: arg count
+ * @av: arg vector
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int ac, char **av)
 {
-int num1 = 5;
-int num2 = 10;
-int sum = add(num1, num2);
-printf("The sum is: %d\n", sum);
+	info_t info[] = { INFO_INIT };
+	int fd = 2;
 
-return (0);
+	asm ("mov %1, %0\n\t"
+			"add $3, %0"
+			: "=r" (fd)
+			: "r" (fd));
+
+	if (ac == 2)
+	{
+		fd = open(av[1], O_RDONLY);
+		if (fd == -1)
+		{
+			if (errno == EACCES)
+				exit(126);
+			if (errno == ENOENT)
+			{
+				_eputs(av[0]);
+				_eputs(": 0: Can't open ");
+				_eputs(av[1]);
+				_eputchar('\n');
+				_eputchar(BUF_FLUSH);
+				exit(127);
+			}
+			return (EXIT_FAILURE);
+		}
+		info->readfd = fd;
+	}
+	populate_env_list(info);
+	read_history(info);
+	hsh(info, av);
+	return (EXIT_SUCCESS);
 }
